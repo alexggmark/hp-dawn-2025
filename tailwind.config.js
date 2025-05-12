210ae68e2df846470a1bbed469178efa5ce70b9e
alexggmark <alexgmark@gmail.com>
2025-05-12 17:39:27 +0100

    Pausing before pull, need to figure out why I'm getting warning messages so much

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,16 +12,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'brand-primary-700': '#00677F',
-        'brand-primary-500': '#0097A9',
-        'brand-primary-100': '#F6FBFF',
-        'brand-secondary-700': '#916969',
-        'brand-secondary-500': '#FCB52D',
-        'brand-background': '#F8F9FB',
-        'brand-border': '#E2E5E8',
-        'brand-border-darker': '#CECECE',
+        'brand-primary-700': '#0F2D64',
+        'brand-primary-500': '#3D72F6',
+        'brand-secondary-700': '#6C7275',
+        'brand-background': '#F1F1F1',
+        'brand-border': '#DADEE2',
         'brand-light': '#ffffff',
         'brand-dark': '#141718'
+      },
+      fontFamily: {
+        'heading-primary': 'var(--font-heading-family)'
       }
     }
   },
