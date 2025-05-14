67664b9ae3414fd166d9062d6b56e2ac68c9240b
alexggmark <alexgmark@gmail.com>
2025-05-14 16:59:20 +0100

    Setting up font basics and creating test section to test, need to look at condensed font again

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,11 +18,23 @@ module.exports = {
         'brand-background': '#F1F1F1',
         'brand-border': '#DADEE2',
         'brand-light': '#ffffff',
-        'brand-dark': '#141718'
+        'brand-dark': '#262626'
       },
       fontFamily: {
         'heading-primary': 'var(--font-heading-family)',
+        'heading-primary-italic': 'var(--font-heading-family-italic)',
         'body-condensed': 'var(--font-body-condensed)'
+      },
+      fontSize: {
+        '7xl': ['80px', { lineHeight: 'auto' }],
+        '6xl': ['72px', { lineHeight: 'auto' }],
+        '5xl': ['62px', { lineHeight: 'auto' }],
+        '4xl': ['49px', { lineHeight: 'auto' }],
+        '3xl': ['38px', { lineHeight: 'auto' }],
+        'xl': ['20px', { lineHeight: 'auto' }],
+        'lg': ['16px', { lineHeight: 'auto' }],
+        'base': ['14px', { lineHeight: 'auto' }],
+        'sm': ['12px', { lineHeight: 'auto' }],
       }
     }
   },
