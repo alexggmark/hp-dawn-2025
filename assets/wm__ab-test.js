fedc1a35f1873a3963a97c95896383d7a788a916
Alex <alexgmark@gmail.com>
2025-11-18 11:38:24 +0000

    Building rotating USP test, adding back in custom_html to quick shop, building ab test related products block, adding in system font

--- assets/wm__ab-test.js
+++ assets/wm__ab-test.js
@@ -3,6 +3,20 @@
 File to controls feature flags in PostHog. This fails safely, if no FeatureFlag running from PostHog, nothing happens.
 Use this file to control variants. I.e. "if test A running, querySelect this element and show/hide"
 
+- data-abtest="experiment-name"
+- data-abtest-var="a" (optional: if comparing 2 features)
+- data-abtest-var="b" (default hidden)
+
+- "active-bundles"
+- "atf-paired"
+- "active-search"
+- "usps"
+- "recently-viewed"
+
+IDEA?: TODO:
+{% capture "original_content" %}
+{% render 'ab-tester', original: original_content, variant: variant %}
+
 */
 
 posthog.onFeatureFlags(function() {
